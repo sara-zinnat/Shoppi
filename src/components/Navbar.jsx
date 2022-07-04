a9b1ee9140e8744b8477695426b92a5e023b7c82
sara-zinnat <shazsara20@gmail.com>
2022-07-03 17:22:14 -0700

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -34,7 +35,7 @@ const SearchContainer = styled.div`
 `;
 const Input = styled.input`
     border: None;
-`
+`;
 const Center = styled.div`
     flex: 1;
     text-align: center;
@@ -45,7 +46,14 @@ const Logo = styled.h1`
 `;
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
 `; 
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+`;
 
 const Navbar = () => {
   return (
@@ -61,7 +69,15 @@ const Navbar = () => {
             <Center>
                 <Logo>Shoppie</Logo>
             </Center>
-            <Right>right</Right> 
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>Sign In</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color = "primary">
+                        <ShoppingCartOutlined/>
+                    </Badge>
+                </MenuItem>
+            </Right> 
         </Wrapper>
     </Container>
   )
